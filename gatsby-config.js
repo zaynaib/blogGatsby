e5b05e81bcd3aa4380e3d7d024dbc984aa522610
Zaynaib Giwa <zaynaibg@gmail.com>
2021-11-02 16:24:48 -0500

    add image plugins for markdown to be processed

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,9 +25,40 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/markdown/blog`,
-        name: `posts`
-      }
+        name: `blog`
+      },
     },
-       `gatsby-transformer-remark`
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
   ]
 }
--- src/markdown/blog/post1.md
+++ src/markdown/blog/post1.md
@@ -5,3 +5,5 @@ title: "Second post"
 ---
 
 With the recent growth of [JAMStack](https://jamstack.wtf/), Static Site Generators  -or SSG for short-  are becoming more popular again.
+
+![myfile](https://user-images.githubusercontent.com/52455330/139071980-91302a8a-37b1-4196-803e-f91b1de2ee5b.gif)
