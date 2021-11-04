2c140675cf3a97ff5a8963fa945de3245061e7ab
Zaynaib Giwa <zaynaibg@gmail.com>
2021-11-04 15:10:49 -0500

    fixed dataviz portfolioPage

---
+++ src/components/dataVizLayout.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+
+export default function Vizlayout(props){
+    return(
+        
+              
+        <div className = "card">
+        <p class="card-header-title is-centered">
+            {props.title}
+        </p>
+          <div className = "card-image">
+            <figure class="image is-4by3">
+              <img src={props.img} alt="Placeholder image">
+              </img>  
+            </figure>
+          </div>
+
+          <div className = "card-content">
+            <div class="content">
+            {props.desc}<br></br>
+            <a className ="button" href={props.link}>Check it out</a>
+        </div>
+          
+          </div>
+
+        </div>
+        
+       
+
+    )
+}
---
+++ src/pages/dataviz.js
@@ -0,0 +1,43 @@
+import * as React from "react"
+import Layout from "../components/main"
+import Vizlayout from "../components/dataVizLayout"
+
+import "../style/mystyles.scss"
+import Basketball from "../images/basketball.png"
+
+export default function Viz() {
+    return (
+        <Layout>
+            
+  
+        <div class="columns">
+            <div class="column">
+            <Vizlayout img ={Basketball} link="https://public.tableau.com/app/profile/zaynaib/viz/Basketball-injuries/Sheet1" title="Baketball Injuries" desc="Tableau Visualization of Injuries of Basketball Players in the NBA"> </Vizlayout>
+
+            </div>
+
+            <div class="column">
+            <Vizlayout img ={Basketball} link="https://public.tableau.com/app/profile/zaynaib/viz/Basketball-injuries/Sheet1" title="Baketball Injuries" desc="Tableau Visualization of Injuries of Basketball Players in the NBA"> </Vizlayout>
+
+            </div>
+            
+      </div>
+
+      <div class="columns">
+      <div class="column">
+      <Vizlayout img ={Basketball} link="https://public.tableau.com/app/profile/zaynaib/viz/Basketball-injuries/Sheet1" title="Baketball Injuries" desc="Tableau Visualization of Injuries of Basketball Players in the NBA"> </Vizlayout>
+
+      </div>
+
+      <div class="column">
+      <Vizlayout img ={Basketball} link="https://public.tableau.com/app/profile/zaynaib/viz/Basketball-injuries/Sheet1" title="Baketball Injuries" desc="Tableau Visualization of Injuries of Basketball Players in the NBA"> </Vizlayout>
+
+      </div>
+      
+</div>
+  
+      
+          
+        </Layout>
+    )
+  }
