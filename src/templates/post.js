578cdf04e7ff8193e08aebbe7429078a611fbd03
Zaynaib Giwa <zaynaibg@gmail.com>
2021-11-04 20:06:37 -0500

    got sticky header working

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import "../style/mystyles.scss"
+import Layout from "../components/main"
+
 
 export default function Post({ data }) {
   const { frontmatter, html } = data.markdownRemark;
 
     return (
+        
+        <Layout>
         <body>
         <div className="post">
-            <h1 className="post-title">{frontmatter.title}</h1>
-            <h2 className="post-date">{frontmatter.date}</h2>
+            <h1 className="title is-2">{frontmatter.title}</h1>
+            <h2 className="title is-4">{frontmatter.date}</h2>
             <div className="post-content" dangerouslySetInnerHTML={{ __html: html }} />
         </div>
         </body>
+        </Layout>
     )
 }
 
